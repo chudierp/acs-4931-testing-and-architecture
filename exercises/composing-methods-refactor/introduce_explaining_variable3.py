8c7cb315e782a5cd0bfd66e460502394c31410d3
Chudier <chudierp@gmail.com>
2022-02-03 11:25:43 -0700

    introduce explaining variable warmup complete

--- exercises/composing-methods-refactor/introduce_explaining_variable3.py
+++ exercises/composing-methods-refactor/introduce_explaining_variable3.py
@@ -1,12 +1,13 @@
 # by Kami Bigdely
 # Extract Variable (alias introduce explaining variable)
-WELL_DONE = 900000
-MEDIUM = 600000
-COOKED_CONSTANT = 0.05
+Well_done = 900000
+Medium = 600000
+Cooked_constant = 0.05
 
-def is_cookeding_criteria_satisfied(time, temperature, pressure, desired_state):
-    if desired_state == 'well-done' and time * temperature * pressure * COOKED_CONSTANT >= WELL_DONE: 
+def is_cooked_criteria_satisfied(time, temperature, pressure, desired_state):
+    current_state = time * temperature * pressure * Cooked_constant
+    if desired_state == 'weel-done' and current_state >= Well_done:
         return True
-    if desired_state == 'medium' and time * temperature * pressure * COOKED_CONSTANT >= MEDIUM:
+    if desired_state == 'medium' and current_state >= Medium:
         return True
     return False
