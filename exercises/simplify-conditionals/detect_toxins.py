42f094fe8280c9acf18825ee64395fd5028f100c
Chudier <chudierp@gmail.com>
2022-02-08 11:37:58 -0700

    exercise twelve complete

--- exercises/simplify-conditionals/detect_toxins.py
+++ exercises/simplify-conditionals/detect_toxins.py
@@ -7,15 +7,8 @@ def make_alert_sound():
 def make_accept_sound():
     print('made acceptance sound')
 
-ingredients = ['sodium benzoate']
-if 'sodium nitrate' in ingredients or 'sodium benzoate' in ingredients\
-or 'sodium oxide' in ingredients:
-    print('!!!')
-    print('there is a toxin in the food!')    
-    print('!!!')
-    make_alert_sound()
-else:
-    print('***')
-    print('Toxin Free')
-    print('***')
-    make_accept_sound()
+    badIngredients = ['sodium benzoate', 'sodium benzoate', 'sodium oxide']
+    if badIngredients:
+        make_alert_sound()
+    else:
+        make_accept_sound()
